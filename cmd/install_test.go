5c8fea2731765e59ca684a6070e3eab7a03ee1e4
Franciszek Walkowiak <franciszek.walkowiak@roche.com>
2022-12-21 10:37:54 +0100

    Download tar.gz files for tests

--- cmd/install_test.go
+++ cmd/install_test.go
@@ -29,13 +29,37 @@ func Test_executeInstallation(t *testing.T) {
 
 func Test_executeInstallationFromTargz(t *testing.T) {
 	t.Skip("skipping integration test")
+	downloadFile(
+		"https://cran.r-project.org/src/contrib/Archive/OrdinalLogisticBiplot/OrdinalLogisticBiplot_0.4.tar.gz",
+		"testdata/targz/OrdinalLogisticBiplot_0.4.tar.gz",
+	)
+	downloadFile(
+		"https://cran.r-project.org/src/contrib/Archive/curl/curl_4.3.2.tar.gz",
+		"testdata/targz/curl_4.3.2.tar.gz",
+	)
+	downloadFile(
+		"https://cran.r-project.org/src/contrib/Archive/bitops/bitops_1.0-6.tar.gz",
+		"testdata/targz/bitops_1.0-6.tar.gz",
+	)
+	downloadFile(
+		"https://cran.r-project.org/src/contrib/Archive/CompQuadForm/CompQuadForm_1.4.2.tar.gz",
+		"testdata/targz/CompQuadForm_1.4.2.tar.gz",
+	)
+	downloadFile(
+		"https://cran.r-project.org/src/contrib/Archive/dotCall64/dotCall64_1.0-1.tar.gz",
+		"testdata/targz/dotCall64_1.0-1.tar.gz",
+	)
+	downloadFile(
+		"https://cran.r-project.org/src/contrib/Archive/tripack/tripack_1.3-9.tar.gz",
+		"testdata/targz/tripack_1.3-9.tar.gz",
+	)
 	cases := []struct{ targz, packageName string }{
 		{"testdata/targz/OrdinalLogisticBiplot_0.4.tar.gz", "OrdinalLogisticBiplot"},
 		{"testdata/targz/curl_4.3.2.tar.gz", "curl"},
-		{"testdata/targz/bitops_1.0-7.tar.gz", "bitops"},
-		{"testdata/targz/CompQuadForm_1.4.3.tar.gz", "CompQuadForm"},
+		{"testdata/targz/bitops_1.0-6.tar.gz", "bitops"},
+		{"testdata/targz/CompQuadForm_1.4.2.tar.gz", "CompQuadForm"},
 		{"testdata/targz/dotCall64_1.0-1.tar.gz", "dotCall64"},
-		{"testdata/targz/tripack_1.3-9.1.tar.gz", "tripack"},
+		{"testdata/targz/tripack_1.3-9.tar.gz", "tripack"},
 	}
 	for _, v := range cases {
 		err := executeInstallation(v.targz, v.packageName, v.packageName+".out")
