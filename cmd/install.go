90c4b4e8ab7c99dd388aedd1b46aa38150c33d3d
Franciszek Walkowiak <franciszek.walkowiak@roche.com>
2023-11-10 16:37:50 +0100

    Update

--- cmd/install.go
+++ cmd/install.go
@@ -154,7 +154,8 @@ func buildPackage(buildPackageChan chan BuildPackageChanInfo, packageName string
 	outputLocation string, buildLogFilePath string, additionalOptions string) {
 	log.Infof("Package %s located in %s is a source package so it has to be built first.",
 		packageName, outputLocation)
-	cmd := "R CMD build " + additionalOptions + " " + outputLocation
+	// TODO allow running R.exe on Windows
+	cmd := "R.exe CMD build " + additionalOptions + " " + outputLocation
 	log.Trace("execCommand:" + cmd)
 	buildLogFile, buildLogFileErr := os.OpenFile(buildLogFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
 	if buildLogFileErr != nil {
@@ -261,7 +262,8 @@ func executeInstallation(outputLocation, packageName, logFilePath, buildLogFileP
 		}
 	}
 
-	cmd := "R CMD INSTALL --no-lock -l " + temporalLibPath + " " + additionalInstallOptions + " " + outputLocation
+	// TODO allow running R.exe on Windows
+	cmd := "R.exe CMD INSTALL --no-lock -l " + temporalLibPath + " " + additionalInstallOptions + " " + outputLocation
 	log.Trace("Executing command:" + cmd)
 	execRCmdInstallChan := make(chan ExecRCmdInstallChanInfo)
 	go executeRCmdInstall(execRCmdInstallChan, cmd, logFile)
